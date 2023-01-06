d7ff5eef2a419ff69c413ef248f8bbc94dd412b2
tobi <tygwak@gabia.com>
2023-01-06 15:49:16 +0900

    init

---
+++ src/test/java/com/gabia/voting/GShareholderVotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabia.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GShareholderVotingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
