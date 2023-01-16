4a9acfb9ffb891aee30cf7708ecc2100502022ff
tobi <tygwak@gabia.com>
2023-01-16 09:56:28 +0900

    add test 환경 분리

--- src/test/java/com/gabia/voting/item/repository/ItemRepositoryTest.java
+++ src/test/java/com/gabia/voting/item/repository/ItemRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles({"test"})
 @DataJpaTest
 public class ItemRepositoryTest {
 
